104b9332db75c9d24ccde2b4ccd92090c46f016a
Adam Harvey <heyitsmeharv@gmail.com>
2022-07-23 10:53:17 +0100

    revert

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ import WaitingRoom from './pages/WaitingRoom';
 import Game from './pages/Game';
 
 const ENDPOINT = "https://rockpaperscissors-be.herokuapp.com/";
-// const socket = io.connect(ENDPOINT);
-const socket = io(ENDPOINT, {
-  withCredentials: true,
-  transports: ['websocket'],
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        "my-custom-header": "abc"
-      }
-    }
-  }
-});
+const socket = io.connect(ENDPOINT);
+// const socket = io(ENDPOINT, {
+//   withCredentials: true,
+//   transports: ['websocket'],
+//   transportOptions: {
+//     polling: {
+//       extraHeaders: {
+//         "my-custom-header": "abc"
+//       }
+//     }
+//   }
+// });
 
 function App() {
   const [isConnected] = useState(socket?.connected);
