1b722fdf4ee4af96340a75f16aea25da3de349b1
Adam Harvey <heyitsmeharv@gmail.com>
2022-08-03 21:56:02 +0100

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Game from './pages/Game';
 const ENDPOINT = "https://rockpaperscissors-be.herokuapp.com/";
 // const socket = io.connect(ENDPOINT);
 const socket = io(ENDPOINT, {
-  transports: ['websocket', 'polling', 'flashsocket'],
+  transports: ['websocket'],
 });
 
 function App() {
